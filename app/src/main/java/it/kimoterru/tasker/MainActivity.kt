8441c2b01de40145e0f459fd0329c3604cc3c4f2
Eskel <emirbajnazarov44@gmail.com>
2022-03-03 22:06:12 +0600

    Initial commit

---
+++ app/src/main/java/it/kimoterru/tasker/MainActivity.kt
@@ -0,0 +1,17 @@
+package it.kimoterru.tasker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import by.kirich1409.viewbindingdelegate.CreateMethod
+import by.kirich1409.viewbindingdelegate.viewBinding
+import it.kimoterru.tasker.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by viewBinding(CreateMethod.INFLATE)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
